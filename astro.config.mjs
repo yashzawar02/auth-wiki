4d33c5575fc8b2417bc3bf1b4ffad0745864769f
Gao Sun <gao@silverhand.io>
2024-10-26 22:19:52 +0800

    feat: render mermaid

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,14 @@
 import { defineConfig } from 'astro/config';
+import rehypeMermaid from 'rehype-mermaid';
+import { rehypeShiki } from '@astrojs/markdown-remark';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  markdown: {
+    rehypePlugins: [
+      [rehypeMermaid, { mermaidConfig: { theme: 'dark' } }],
+      [rehypeShiki, { theme: 'one-dark-pro' }]
+    ],
+    syntaxHighlight: false
+  }
+});

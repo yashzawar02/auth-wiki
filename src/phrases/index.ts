939ee9a67bf2445d235017957d66dae121a88e3a
Gao Sun <gao@silverhand.io>
2024-11-06 21:19:49 +0800

    feat: language selector (#34)
    * feat: language selector
    
    * refactor: improve comments
    
    * refactor: add language selector in topbar

--- src/phrases/index.ts
+++ src/phrases/index.ts
@@ -4,6 +4,12 @@ export * from './zh';
 import { en } from './en';
 import { zh } from './zh';
 
+/** Display names for each locale. */
+export const localeDisplay: Readonly<Record<string, string>> = Object.freeze({
+  en: 'English',
+  zh: '简体中文',
+});
+
 export const getPhrases = (locale: string) => {
   switch (locale) {
     case 'zh':
